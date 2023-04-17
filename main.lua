b7bdef4559d4449c9cd7c36a750e27ecbf59d6c2
Bug <77031032+0zBug@users.noreply.github.com>
2023-04-16 20:07:00 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,19 @@
+makefolder("🔑")
+
+return setmetatable({
+    get = function(key)
+        local file = string.format("🔑/%s.dat", key)
+                
+        return isfile(file) and readfile(file) or nil
+    end,
+    set = function(key, value)
+        writefile(string.format("🔑/%s.dat", key), value)
+    end
+    delete = function(key)
+        delfile(string.format("🔑/%s.dat", key))
+    end
+}, {
+    __index = function(self, index)
+        return rawget(self, string.lower(index))
+    end
+})
